10eeed835816da9df244b8a7ac6ddaafccb1d517
AsciiMoth <ascii@moth.contact>
2025-08-31 13:26:13 +0400

    chore: setup flake env

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Deb/rpm repo";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.dpkg
+          pkgs.apt
+          pkgs.gzip
+        ];
+      };
+    });
+}
