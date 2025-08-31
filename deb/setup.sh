e001a18c48ae7477914abdd3b3b6f7169c9155f8
AsciiMoth <ascii@moth.contact>
2025-08-31 15:37:35 +0400

    feat: add setup.sh

---
+++ deb/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl "https://asciimoth.github.io/testrepo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/asciimoth-testrepo.gpg
+curl https://asciimoth.github.io/testrepo/deb/list_file.list | sudo tee /etc/apt/sources.list.d/asciimoth-testrepo.list
+sudo apt update
+
