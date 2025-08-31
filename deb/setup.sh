cbfb5a3482ae1f81278232974d21d6e1f3a3eb8e
AsciiMoth <ascii@moth.contact>
2025-08-31 15:45:00 +0400

    fix: remove garbadge output from setup.sh

--- deb/setup.sh
+++ deb/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl "https://asciimoth.github.io/testrepo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/asciimoth-testrepo.gpg
-curl https://asciimoth.github.io/testrepo/deb/list_file.list | sudo tee /etc/apt/sources.list.d/asciimoth-testrepo.list
+curl "https://asciimoth.github.io/testrepo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/asciimoth-testrepo.gpg > /dev/null
+curl https://asciimoth.github.io/testrepo/deb/list_file.list | sudo tee /etc/apt/sources.list.d/asciimoth-testrepo.list > /dev/null
 sudo apt update
 
